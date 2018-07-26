754bee26d4a90e3686b1aa290cd062a342e6b2c3
moskowitza <moskowitzaaron@mac.com>
2018-07-26 09:41:46 -0400

    opacity: 0.5

--- arrays.js
+++ arrays.js
@@ -1,8 +1,26 @@
-function createNewPerson(name) {
-    var obj = {};
-    obj.name = name;
-    obj.greeting = function() {
-      alert('Hi! I\'m ' + this.name + '.');
-    };
-    return obj;
-  }
+// function Person(first, last, age, gender, interests) {
+//     this.name = {
+//       'first': first,
+//       'last' : last
+//     };
+//     this.age = age;
+//     this.gender = gender;
+//     this.interests = interests;
+//     this.bio = function() {
+//       alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+//     };
+//     this.greeting = function() {
+//       console.log('Hi! I\'m ' + this.name.first + '.');
+//     };
+//   }
+// var person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
+
+// person1.greeting();
+// console.log(JSON.stringify(person1.__proto__));
+let result = ["a","b","c"];
+let guess="c";
+function guessArrBuild(el){
+  if (result.indexOf(el) === -1) result.push(el)
+}
+myfunction(guess);
+console.log(result);
