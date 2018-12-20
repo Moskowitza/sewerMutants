c647aae52f121c962f078e093d4ac1fa2afe4b30
moskowitza <moskowitzaaron@mac.com>
2018-12-20 15:14:20 -0500

    gulp problems

--- stylusPractice/gulpfile.js
+++ stylusPractice/gulpfile.js
@@ -1,14 +1,21 @@
 const gulp = require("gulp");
 const stylus = require("gulp-stylus");
+const typographic = require("typographic");
+const nib = require("nib");
+const rupture = require("rupture");
 
 gulp.task("styles", () => {
   gulp
     .src("style.styl")
-    .pipe(stylus())
+    .pipe(
+      stylus({
+        use: [typographic(), nib(), rupture()]
+      })
+    )
     .pipe(gulp.dest("./"));
 });
 gulp.task("watch:styles", () => {
   gulp.watch("**/*.styl", gulp.parallel("styles"));
 });
 
-// to run : npm run gulp styles
+// to run : npm run gulp watch:styles
