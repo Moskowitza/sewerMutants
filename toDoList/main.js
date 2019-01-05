371d4cb383e5f239d8993338714997902fbd1578
moskowitza <moskowitzaaron@mac.com>
2019-01-05 12:39:54 -0500

    Event Bubbling to remove child node

---
+++ toDoList/main.js
@@ -0,0 +1,7 @@
+const list = document.querySelector("#book-list ul");
+list.addEventListener("click", function(e) {
+  if (e.target.className == "delete") {
+    const li = e.target.parentElement;
+    list.removeChild(li);
+  }
+});
