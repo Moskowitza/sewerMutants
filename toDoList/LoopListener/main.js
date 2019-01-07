7f764df766c6be82fcfeffc97ab16980cf3156b7
moskowitza <moskowitzaaron@mac.com>
2019-01-07 15:38:56 -0500

    cleaning up

--- toDoList/LoopListener/main.js
+++ toDoList/LoopListener/main.js
@@ -1,22 +1,22 @@
 // add a new list item
 const addForm = document.forms[0];
 // our list
-const ToDos = document.forms['to-dos'];
+const ToDos = document.forms["to-dos"];
 
-addForm.addEventListener('submit', e => {
+addForm.addEventListener("submit", e => {
   e.preventDefault();
   //   Here we grab value using the id Selector, not by looking down the DOM Tree
-  const toDoItem = document.querySelector('#to-do').value.toString();
+  const toDoItem = document.querySelector("#to-do").value.toString();
   if (toDoItem.length > 0) {
-    const blockContainer = document.createElement('div');
-    blockContainer.setAttribute('class', 'to-do-item');
-    blockContainer.style.display = 'block';
-    const input = document.createElement('input');
-    const label = document.createElement('label');
+    const blockContainer = document.createElement("div");
+    blockContainer.setAttribute("class", "to-do-item");
+    blockContainer.style.display = "block";
+    const input = document.createElement("input");
+    const label = document.createElement("label");
     // Now set some attributes for the input to be a checkbox
-    input.setAttribute('type', 'checkbox');
-    input.setAttribute('id', toDoItem);
-    label.setAttribute('for', toDoItem);
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("id", toDoItem);
+    label.setAttribute("for", toDoItem);
     label.innerText = toDoItem;
     // Append to dom
 
@@ -30,11 +30,11 @@ addForm.addEventListener('submit', e => {
 
 // remove when checked
 
-ToDos.addEventListener('submit', e => {
+ToDos.addEventListener("submit", e => {
   // prevent the page from reloading, redirecting
   e.preventDefault();
   //   get an array of our to-do divs
-  const listArray = Array.from(ToDos.querySelectorAll('.to-do-item'));
+  const listArray = Array.from(ToDos.querySelectorAll(".to-do-item"));
   listArray.forEach(item => {
     const checkbox = item.querySelector("input[type='checkbox']");
     const thisDiv = checkbox.parentNode;
