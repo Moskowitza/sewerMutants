7bb338208b8fbf7b0a656d2f7f6b4be9e9fcd747
moskowitza <moskowitzaaron@mac.com>
2019-01-15 15:30:40 -0500

    use an iife to hide methods from global name space

--- modules/js/dom.js
+++ modules/js/dom.js
@@ -1,4 +1,23 @@
-function domWrapper() {
+// function domWrapper() {
+//   function addUserToDom(name) {
+//     const node = document.createElement("li");
+//     const text = document.createTextNode(name);
+//     node.appendChild(text);
+//     document.getElementById("users").appendChild(node);
+//   }
+//   document.getElementById("submit").addEventListener("click", () => {
+//     const input = document.getElementById("input");
+//     addUserToDom(input.value);
+//     input.value = "";
+//   });
+//   var users = APP.getUsers();
+//   for (let i = 0; i < users.length; i += 1) {
+//     addUserToDom(users[i]);
+//   }
+// }
+// domWrapper();
+
+(function() {
   function addUserToDom(name) {
     const node = document.createElement("li");
     const text = document.createTextNode(name);
@@ -14,5 +33,4 @@ function domWrapper() {
   for (let i = 0; i < users.length; i += 1) {
     addUserToDom(users[i]);
   }
-}
-domWrapper();
+});
--- modules/js/users.js
+++ modules/js/users.js
@@ -1,8 +1,18 @@
-function usersWrapper() {
+//create an IIFE from the code
+
+// function usersWrapper() {
+//     var users = ["Eno", "Toki", "Blue"];
+//     function getUsers() {
+//       return users;
+//     }
+//     APP.getUsers = getUsers;
+// }
+// usersWrapper();
+
+(function() {
   var users = ["Eno", "Toki", "Blue"];
   function getUsers() {
     return users;
   }
   APP.getUsers = getUsers;
-}
-usersWrapper();
+})();
