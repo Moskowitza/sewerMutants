8961e0cb138683ed33eb85f5a7924b6b83768123
moskowitza <moskowitzaaron@mac.com>
2019-01-25 13:03:52 -0500

    hamberder manyous

---
+++ TrendySaleSite/main.js
@@ -0,0 +1,12 @@
+const hamberder = document.querySelector('.hamberder');
+function myFunction() {
+  console.log('loaded script');
+  // const menu = document.querySelector('.LeftLinks');
+  // console.log('loaded script');
+  // if (menu.className === 'LeftLinks') {
+  //   menu.className += ' responsive';
+  // } else {
+  //   menu.className = 'LeftLinks';
+  // }
+}
+hamberder.addEventListener('click', myFunction);
